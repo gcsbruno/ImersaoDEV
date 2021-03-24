8962e93ba49d31044ffd830ca3c3c42fb9aad9e0
gcsbruno <brunoguimaraes.cs@gmail.com>
2021-03-24 10:35:29 -0300

    Dia 3

--- Day3/app.js
+++ Day3/app.js
@@ -1,10 +1,22 @@
-const numeroSecreto = 3
+var numeroSecreto = parseInt(Math.random() * 10)
+var tentativas = 3
 
-const chute = parseInt(prompt("Digite um número entre 0 e 10"))
+while (tentativas > 0) {
 
-if (numeroSecreto == chute)
-{
-    alert ("Acertou!")
-} else {
-    alert("Errou. O número secreto era " + numeroSecreto)
+        var chute = parseInt(prompt("Digite um número entre 0 e 10"))
+
+        if (numeroSecreto == chute) {
+            alert("Acertou!")
+            break
+        } else if (chute > numeroSecreto) {
+            alert("o número secreto é menor")
+            tentativas = tentativas - 1
+        } else if (chute < numeroSecreto) {
+            alert("o número secreto é maior")
+            tentativas = tentativas - 1
+        } 
+}
+
+if (chute != numeroSecreto) {
+    alert("Suas tentativas acabaram. O número secreto era " + numeroSecreto)
 }
